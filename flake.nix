d1b7dd6e3528ddd7ac98b1c4943ba54eef79ae8f
Emp. Justine <1224205+empjustine@users.noreply.github.com>
2023-11-30 23:45:54 -0300

    github:NixOS/nixpkgs/67be70a859530f6f7c358568eaa6ab0d84b36b01

--- flake.nix
+++ flake.nix
@@ -5,14 +5,19 @@
     nixpkgs-stable,
     ...
   } @ inputs: {
-    packages.x86_64-linux.execline = inputs.nixpkgs-stable.legacyPackages.x86_64-linux.buildEnv {
-      name = "execlineb";
-      paths = [];
-      postBuild = ''
-        mkdir -p $out/bin
-        ln -s ${inputs.nixpkgs-stable.legacyPackages.x86_64-linux.execline}/bin/execlineb $out/bin/
-        ln -s ${inputs.nixpkgs-stable.legacyPackages.x86_64-linux.execline}/bin/execline-cd $out/bin/
-      '';
+    packages.x86_64-linux = let
+      pkgs = nixpkgs-stable.legacyPackages.x86_64-linux;
+    in {
+      execline = pkgs.buildEnv {
+        name = "execlineb";
+        paths = [];
+        postBuild = ''
+          mkdir -p $out/bin
+          ln -s ${pkgs.execline}/bin/execlineb $out/bin/
+          ln -s ${pkgs.execline}/bin/execline-cd $out/bin/
+        '';
+      };
     };
+    default = self.packages.x86_64-linux.execline;
   };
 }
